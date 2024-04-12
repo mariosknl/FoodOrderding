7c73b7c90befb8c3bfda9ab849c966642629536f
mariosknl <mariosknl@outlook.com>
2024-04-12 14:15:08 +0300

    adding total amount in the cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,17 +1,23 @@
+import Button from "@/components/Button";
 import CartListItem from "@/components/CartListItem";
 import { useCart } from "@providers/CartProvider";
 import { StatusBar } from "expo-status-bar";
 import { FlatList, Platform, Text, View } from "react-native";
 const CartScreen = () => {
-	const { items } = useCart();
+	const { items, total } = useCart();
 
 	return (
-		<View>
+		<View style={{ padding: 10 }}>
 			<FlatList
 				data={items}
 				renderItem={({ item }) => <CartListItem cartItem={item} />}
-				contentContainerStyle={{ padding: 10, gap: 10 }}
+				contentContainerStyle={{ gap: 10 }}
 			/>
+
+			<Text style={{ marginTop: 10, fontSize: 20, fontWeight: "500" }}>
+				Total: € {total}
+			</Text>
+			<Button text="Checkout" />
 
 			{/* Use a light status bar on iOS to account for the black space above the modal */}
 			<StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
