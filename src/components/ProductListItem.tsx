a2facc1bb03202de2b5452b0f2a8cf6f846dcbec
mariosknl <mariosknl@outlook.com>
2024-04-12 08:23:23 +0300

    flatlist

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -15,6 +15,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
 			<Image
 				source={{ uri: product.image ?? defaultPizzaImage }}
 				style={styles.image}
+				resizeMode="contain"
 			/>
 			<Text style={styles.title}>{product.name}</Text>
 			<Text style={styles.price}>€ {product.price}</Text>
@@ -27,6 +28,8 @@ const styles = StyleSheet.create({
 		backgroundColor: Colors.light.background,
 		padding: 10,
 		borderRadius: 20,
+		flex: 1,
+		maxWidth: "50%",
 	},
 
 	image: {
