a13c89402fe518cbcdd119f7b173bd6aff24d477
mariosknl <mariosknl@outlook.com>
2024-04-12 14:59:39 +0300

    create a product

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,11 +1,15 @@
 import Button from "@/components/Button";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Colors from "@/constants/Colors";
 import { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { Stack } from "expo-router";
 const CreateProductScreen = () => {
 	const [name, setName] = useState("");
 	const [price, setPrice] = useState("");
-
 	const [errors, setErrors] = useState("");
+	const [image, setImage] = useState<string | null>(null);
 
 	const resetFields = () => {
 		setName("");
@@ -40,9 +44,30 @@ const CreateProductScreen = () => {
 
 		resetFields();
 	};
+	const pickImage = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			setImage(result.assets[0].uri);
+		}
+	};
 
 	return (
 		<View style={styles.container}>
+			<Stack.Screen options={{ title: "Create Product" }} />
+			<Image
+				source={{ uri: image ?? defaultPizzaImage }}
+				style={styles.image}
+			/>
+			<Text onPress={pickImage} style={styles.textButton}>
+				Select Image
+			</Text>
+
 			<Text style={styles.label}>Name</Text>
 			<TextInput
 				value={name}
@@ -82,6 +107,17 @@ const styles = StyleSheet.create({
 		borderRadius: 5,
 		marginTop: 5,
 		marginBottom: 20,
+	},
+	image: {
+		width: "50%",
+		aspectRatio: 1,
+		alignSelf: "center",
+	},
+	textButton: {
+		alignSelf: "center",
+		fontWeight: "bold",
+		color: Colors.light.tint,
+		marginVertical: 10,
 	},
 });
 
