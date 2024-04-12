2371837256518e051951ae144be0ed871a0de1ff
mariosknl <mariosknl@outlook.com>
2024-04-12 08:52:07 +0300

    product details screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,44 @@
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Colors from "@/constants/Colors";
+import products from "@assets/data/products";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 const ProductDetailsScreen = () => {
 	const { id } = useLocalSearchParams();
 
-	return (
-		<View>
-			<Stack.Screen options={{ title: "Details" }} />
+	const product = products.find((p) => p.id.toString() === id);
 
-			<Text>ProductDetailsScreen for id: {id}</Text>
+	if (!product) {
+		return <Text>Product not found</Text>;
+	}
+
+	return (
+		<View style={styles.container}>
+			<Stack.Screen options={{ title: product.name }} />
+			<Image
+				source={{ uri: product.image ?? defaultPizzaImage }}
+				style={styles.image}
+			/>
+
+			<Text style={styles.price}>€ {product.price}</Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: Colors.light.background,
+		flex: 1,
+		padding: 10,
+	},
+	image: {
+		width: "100%",
+		aspectRatio: 1,
+	},
+	price: {
+		fontSize: 18,
+		fontWeight: "bold",
+	},
+});
+
 export default ProductDetailsScreen;
