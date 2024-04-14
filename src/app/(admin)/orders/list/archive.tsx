47c3a9450d1ed7f25159952813958ab7f694bce7
mariosknl <mariosknl@outlook.com>
2024-04-14 08:15:17 +0300

    update order status

--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -1,7 +1,7 @@
 // import { FlatList } from "react-native-gesture-handler";
 import { FlatList } from "react-native";
-import orders from "../../../../assets/data/orders";
-import OrderListItem from "../../../components/OrderListItem";
+import orders from "../../../../../assets/data/orders";
+import OrderListItem from "../../../../components/OrderListItem";
 import { Stack } from "expo-router";
 
 export default function OrdersScreen() {
